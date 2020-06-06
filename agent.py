034a7ab94d66063f8f99a301270a2b20bb02beec
loquebot <loquebot1@gmail.com>
2020-06-06 10:22:34 +0200

    Init model usage for action prediction

--- agent.py
+++ agent.py
@@ -1,13 +1,22 @@
 import numpy as np
 
-from constants import action_size
+
 
 class Agent():
-  def __init__(self):
-    pass
+  def __init__(self, state_size, action_size):
+    self.action_size = action_size
+    self.epsilon = 1.0
+    self.model = mlp(state_size, action_size)
 
   def act(self, state):
-    return np.random.choice(action_size)
+    if np.random.rand() <= self.epsilon:
+      return np.random.choice(self.action_size)
+    act_values = self.model.predict(state)
+    return np.argmax(act_values[0]) # returns action
 
   def update_replay_memory(self, state, action, reward, next_state):
     pass
+
+
+def mlp(input_dim, n_action, n_hidden_layers=1, hidden_dim=32):
+  return None # todo
--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import stateprocessor
 from agent import Agent
 from api import APIThread
+from constants import action_size
 from env import Env
 from stateprocessor import Stateprocessor
 
 if __name__ == '__main__':
-  agent = Agent()
+  state_size = 0 # todo
+  agent = Agent(state_size=state_size, action_size=action_size)
   env = Env()
   stateprocessor.stateprocessor = Stateprocessor(agent, env, is_train=True)
 
